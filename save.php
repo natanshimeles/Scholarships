c1697ee02ed931385b4349701ea1082bd8c6b7be
natanshimeles <tesfayenatan@yahoo.com>
2017-12-14 22:29:13 +0800

    last commit

--- save.php
+++ save.php
@@ -1,4 +1,59 @@
 <?php
+require 'Conn.php';
+
+$catagory ="ALL";
+$university ="nuaa";
+$degree= "masters";
+$language="english";
+$semester="fall";
+$program="computer Science";
+
+if($catagory=="ALL"){
+	if ($university =="ALL"){
+		#contine from here
+		
+
+
+	}
+	else if ($degree= ="ALL") {
+		# code...
+	}
+	elseif ($language=="ALL") {
+	# code...
+    }
+     else if ($semester=="ALL") {
+	# code...
+   }
+   else if ($program=="ALL") {
+	# code...
+   }
+   else{
+
+   	echo "error occured";
+   }
+
+
+}
+else if ($university =="ALL") {
+	# code...
+}
+else if ($degree= ="ALL") {
+	# code...
+}
+elseif ($language=="ALL") {
+	# code...
+}
+else if ($semester=="ALL") {
+	# code...
+}
+else if ($program=="ALL") {
+	# code...
+}
+else{
+	$query = "select * from scholars";
+
+
+}
 
 
 
