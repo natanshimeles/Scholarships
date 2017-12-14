c981f41df891787e593a13744bd579d13abf6378
natanshimeles <tesfayenatan@yahoo.com>
2017-12-14 22:00:18 +0800

    last commit

---
+++ Conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername ="localhost";
+$username = "root";
+$password = "";
+$database = "lectureprogram";
+$conn=new mysqli($servername,$username,$password,$database) or die("cant connect");
+
+
+
+
+
+?>
